441c5be18ffcd2a8b56c7e538635b3bc11f51527
WILLWANG <wangziweichn@outlook.com>
2019-05-14 01:16:20 +0800

    完成部分页面

--- main.js
+++ main.js
@@ -6,43 +6,49 @@ let $lastcopy = $images.eq($images.length - 1).clone(true)
 
 $slides.append($firstcopy)
 $slides.prepend($lastcopy)
-$slides.css({transform:'translateX(-600px)'})
+$slides.css({transform:'translateX(-920px)'})
 
 let current = 0
 
 $buttons.eq(0).on('click',function(){
-    if(current === 2){
-        $slides.css({transform:'translateX(-2400px)'})
+    if(current === 3){
+        $slides.css({transform:'translateX(-4600px)'})
             .one('transitionend',function(){
                 $slides.hide()
-                .offset()
-                $slides.css({transform:'translateX(-600px)'})
-                .show()
+                $slides.offset()
+                $slides.css({transform:'translateX(-920px)'})
+                $slides.show()
             })
     }else{
-        $slides.css({transform:'translateX(-600px)'})
+        $slides.css({transform:'translateX(-920px)'})
     }
     current = 0
 })
 
 $buttons.eq(1).on('click',function(){
     
-    $slides.css({transform:'translateX(-1200px)'})
+    $slides.css({transform:'translateX(-1840px)'})
     current = 1
 })
 
 $buttons.eq(2).on('click',function(){
+    
+    $slides.css({transform:'translateX(-2760px)'})
+    current = 2
+})
+
+$buttons.eq(3).on('click',function(){
     if(current === 0){
         $slides.css({transform:'translateX(0px)'})
             .one('transitionend',function(){
                 $slides.hide()
                 .offset()
-                $slides.css({transform:'translateX(-1800px)'})
+                $slides.css({transform:'translateX(-3680px)'})
                 .show()
             })
     }else{
-        $slides.css({transform:'translateX(-1800px)'})
+        $slides.css({transform:'translateX(-3680px)'})
     }
-    current = 2
+    current = 3
 })
 
