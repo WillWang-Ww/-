4970adc5323d1824eb3321d35655bede6e8251be
WILLWANG <wangziweichn@outlook.com>
2019-05-14 23:46:57 +0800

    增加三角形样式

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let $slides = $('#slides')
 let $images = $slides.children('img')
 let $firstcopy = $images.eq(0).clone(true)
 let $lastcopy = $images.eq($images.length - 1).clone(true)
+let $traingle = $('.triangle')
 
 $slides.append($firstcopy)
 $slides.prepend($lastcopy)
@@ -16,6 +17,8 @@ $buttons.eq(0).on('click',function(){
     $buttons.eq(3).removeClass('active')
     $buttons.eq(0).removeClass('hover')
     $buttons.eq(0).addClass('active')
+    $traingle.css({display:'none'})
+    $traingle.eq(0).css({display:'inline-block'})
     if(current === 3){
         $slides.css({transform:'translateX(-4600px)'})
             .one('transitionend',function(){
@@ -36,6 +39,8 @@ $buttons.eq(1).on('click',function(){
     $buttons.eq(3).removeClass('active')
     $buttons.eq(1).removeClass('hover')
     $buttons.eq(1).addClass('active')
+    $traingle.css({display:'none'})
+    $traingle.eq(1).css({display:'inline-block'})
     $slides.css({transform:'translateX(-1840px)'})
     current = 1
 })
@@ -46,6 +51,8 @@ $buttons.eq(2).on('click',function(){
     $buttons.eq(3).removeClass('active')
     $buttons.eq(2).removeClass('hover')
     $buttons.eq(2).addClass('active')
+    $traingle.css({display:'none'})
+    $traingle.eq(2).css({display:'inline-block'})
     $slides.css({transform:'translateX(-2760px)'})
     current = 2
 })
@@ -56,6 +63,8 @@ $buttons.eq(3).on('click',function(){
     $buttons.eq(0).removeClass('active')
     $buttons.eq(3).removeClass('hover')
     $buttons.eq(3).addClass('active')
+    $traingle.css({display:'none'})
+    $traingle.eq(3).css({display:'inline-block'})
     if(current === 0){
         $slides.css({transform:'translateX(0px)'})
             .one('transitionend',function(){
